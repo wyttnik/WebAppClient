589b79e78c3e0f4e457c9c5379cec661e9823a89
wyttnik <thevladiinoy@gmail.com>
2023-06-21 03:51:31 +0300

    Added some .js functionality to the site

--- BooksApp/wwwroot/js/site.js
+++ BooksApp/wwwroot/js/site.js
@@ -2,31 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-//Get the button
-let mybutton = document.getElementById("btn-back-to-top");
-if (mybutton != null) {
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function () {
-        scrollFunction();
-    };
-
-    function scrollFunction() {
-        if (
-            document.body.scrollTop > 20 ||
-            document.documentElement.scrollTop > 20
-        ) {
-            mybutton.style.display = "block";
-        } else {
-            mybutton.style.display = "none";
-        }
-    }
-    // When the user clicks on the button, scroll to the top of the document
-    mybutton.addEventListener("click", backToTop);
-
-    function backToTop() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    }
-}
-
